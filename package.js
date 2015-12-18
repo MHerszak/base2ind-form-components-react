8c8b5b2edbed4869be87352187bdeb8123a55873
michelherszak <michel.herszak@gmail.com>
2015-12-18 01:56:29 -0700

    Bug fix

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'browserstudios:form-components-react',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'This package includes simple from elements written in React',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/MHerszak/base2ind-form-components-react.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
